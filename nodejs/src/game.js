2d7f37474dca0505b9095e6485d938c8d08ecd60
Alexander Kolodyazhniy <aleksandr.kolodyazhnyi@ssa.group>
2018-10-12 18:48:30 +0300

    Game run process removed to separate file

--- nodejs/src/game.js
+++ nodejs/src/game.js
@@ -157,26 +157,4 @@ var Game = function () {
   };
 };
 
-var notAWinner = false;
-
-var game = new Game();
-
-game.add('Chet');
-game.add('Pat');
-game.add('Sue');
-
-var isReady = game.init();
-
-do {
-
-  game.roll(Math.floor(Math.random() * 6) + 1);
-
-  if (Math.floor(Math.random() * 10) == 7) {
-    notAWinner = game.wrongAnswer();
-  } else {
-    notAWinner = game.wasCorrectlyAnswered();
-  }
-
-} while (notAWinner);
-
 module.exports = Game;
