9af13c9fd683633393a29e8c5eeb3f884d7d93a5
Alexander Kolodyazhniy <aleksandr.kolodyazhnyi@ssa.group>
2018-10-16 18:00:19 +0300

    minor syntactic fixes

--- nodejs/src/game.js
+++ nodejs/src/game.js
@@ -38,25 +38,25 @@ var Game = function () {
     return players.length >= MIN_PLAYERS_NUMBER;
   };
 
-  this.add = function (playerName) {
-    var player = new Player(playerName);
-    players.push(player);
+  this.add = function ( playerName ) {
+    var player = new Player( playerName );
+    players.push( player );
 
-    console.log(playerName + " was added");
-    console.log("They are player number " + players.length);
+    console.log( playerName + " was added" );
+    console.log( "They are player number " + players.length );
 
     return true;
   };
 
   this.init = function () {
-    if (players.length) {
+    if ( players.length ) {
       currentPlayerIndex = 0;
       currentPlayer = players[currentPlayerIndex];
 
-      for (var i = 0; i < CATEGORY_QUESTIONS_NUMBER; i++) {
-        questionCategories.forEach(function(category) {
-          questions[category].push(category + " Question " + i);
-        });
+      for ( var i = 0; i < CATEGORY_QUESTIONS_NUMBER; i++ ) {
+        questionCategories.forEach( function ( category ) {
+          questions[category].push( category + " Question " + i );
+        } );
       };
 
       return true;
@@ -73,7 +73,7 @@ var Game = function () {
     console.log( questionCollection.shift() );
   };
 
-  var movePlayer = function (player, steps) {
+  var movePlayer = function ( player, steps ) {
     player.place = ( player.place + steps ) % PLACES_NUMBER;
   }
 
@@ -82,40 +82,40 @@ var Game = function () {
     currentPlayer = players[currentPlayerIndex];
   };
 
-  var getOutOfPenaltyBox = function (roll) {
+  var getOutOfPenaltyBox = function ( roll ) {
     isGettingOutOfPenaltyBox = true;
-    if (!currentPlayer.inPenaltyBox) {
+    if ( !currentPlayer.inPenaltyBox ) {
       return;
     }
-    if (roll % 2 === 0) {
-      console.log(currentPlayer.name + " is not getting out of the penalty box");
+    if ( roll % 2 === 0 ) {
+      console.log( currentPlayer.name + " is not getting out of the penalty box" );
       isGettingOutOfPenaltyBox = false;
     } else {
-      console.log(currentPlayer.name + " is getting out of the penalty box");
+      console.log( currentPlayer.name + " is getting out of the penalty box" );
     }
   };
 
-  this.roll = function (roll) {
-    console.log(currentPlayer.name + " is the current player");
-    console.log("They have rolled a " + roll);
+  this.roll = function ( roll ) {
+    console.log( currentPlayer.name + " is the current player" );
+    console.log( "They have rolled a " + roll );
 
-    getOutOfPenaltyBox(roll);
+    getOutOfPenaltyBox( roll );
 
     if ( isGettingOutOfPenaltyBox ) {
-      movePlayer(currentPlayer, roll);
-      console.log(currentPlayer.name + "'s new location is " + currentPlayer.place);
-      console.log("The category is " + currentCategory());
+      movePlayer( currentPlayer, roll );
+      console.log( currentPlayer.name + "'s new location is " + currentPlayer.place );
+      console.log( "The category is " + currentCategory() );
       askQuestion();
     }
   };
 
   this.wasCorrectlyAnswered = function () {
     var notAWinner = true;
-    if (isGettingOutOfPenaltyBox) {
-      console.log('Answer was correct!!!!');
+    if ( isGettingOutOfPenaltyBox ) {
+      console.log( 'Answer was correct!!!!' );
       currentPlayer.purse += 1;
-      console.log(currentPlayer.name + " now has " +
-        currentPlayer.purse + " Gold Coins.");
+      console.log( currentPlayer.name + " now has " +
+        currentPlayer.purse + " Gold Coins." );
 
       notAWinner = !didPlayerWin();
     }
@@ -125,8 +125,8 @@ var Game = function () {
   };
 
   this.wrongAnswer = function () {
-    console.log('Question was incorrectly answered');
-    console.log(currentPlayer.name + " was sent to the penalty box");
+    console.log( 'Question was incorrectly answered' );
+    console.log( currentPlayer.name + " was sent to the penalty box" );
     currentPlayer.inPenaltyBox = true;
 
     nextPlayer();
